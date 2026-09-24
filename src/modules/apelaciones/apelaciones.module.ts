eb15e2157bc0f46f6986902dd9137c30b0919ad0
JeycomCaballero <jeycomgonzales@gmail.com>
2026-09-23 19:30:35 -0500

    fix: modulo de apelaciones

--- src/modules/apelaciones/apelaciones.module.ts
+++ src/modules/apelaciones/apelaciones.module.ts
@@ -4,9 +4,10 @@ import { Apelaciones } from '../../entities/Apelaciones';
 import { Usuarios } from '../../entities/Usuarios';
 import { ApelacionesService } from './apelaciones.service';
 import { ApelacionesController } from './apelaciones.controller';
+import { CommonModule } from '../../common/common.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Apelaciones, Usuarios])],
+  imports: [TypeOrmModule.forFeature([Apelaciones, Usuarios]), CommonModule],
   controllers: [ApelacionesController],
   providers: [ApelacionesService],
   exports: [ApelacionesService],
